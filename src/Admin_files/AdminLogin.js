8db91611153e6ece9c59096e8308edf4ba9e12d6
senthamizhanB <senthamizhan1234@gmail.com>
2023-05-23 13:48:05 +0530

    syntax error removed

--- src/Admin_files/AdminLogin.js
+++ src/Admin_files/AdminLogin.js
@@ -1,77 +1,103 @@
-import React, { Component } from 'react'
-import { server_url } from '../api';
-const axios = require('axios');
-const localStorage = require('local-storage')
+import React, { Component } from "react";
+import { server_url } from "../api";
+const axios = require("axios");
+const localStorage = require("local-storage");
 class AdminLogin extends Component {
-    constructor() {
-        super();
-        this.state = {
-            uname: '',
-            pword: ''
-        };
-    }
-    onChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-    onSubmit = (e) => {
-        e.preventDefault();
-        const { uname, pword } = this.state;
-        axios.post(server_url + '/api/users/login-admin', { username: uname, password: pword }).then(
-            function (response) {
-                if (response.status == 200) {
-                    window.location = "/adminverify"
-                }
-                try {
-                    localStorage.set('authtoken', response.data.token)
-                }
-                catch (error) {
-                    console.log(error)
-                }
-            }
-        )
-
-    }
-    render() {
-        const { uname, pword } = this.state;
-        return (
-
-            <div >
-                <div>
-                    <div >
-
-                        <div >
-                            <br />
-                            <br />
-                            <center><h3>Admin Login</h3></center>
-                            <br />
-                            <br />
-                        </div>
-
-                        <div >
-                            <div >
-                                <center><label style={{ paddingRight: `20px` }} for="username">Username</label>
-                                    <input type="text" name="uname" value={uname} onChange={this.onChange} autoComplete="off" /></center>
-                            </div><br />
-
-                            <div>
-                                <center><label style={{ paddingRight: `20px` }} for="password">Password</label>
-                                    <input type="password" id="password" name="pword" autoComplete="off" value={pword} onChange={this.onChange} /></center>
-                            </div><br />
-
-                            <div >
-                                <p >
-
-                                    <center><button onClick={this.onSubmit} >Login</button></center>
-                                </p>
-                            </div><br />
-                        </div>
-
-                    </div>
-                </div>
+  constructor() {
+    super();
+    this.state = {
+      uname: "",
+      pword: "",
+    };
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { uname, pword } = this.state;
+    axios
+      .post(server_url + "/api/users/login-admin", {
+        username: uname,
+        password: pword,
+      })
+      .then(function (response) {
+        if (response.status == 200) {
+          window.location = "/adminverify";
+        }
+        try {
+          localStorage.set("authtoken", response.data.token);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+  };
+  render() {
+    const { uname, pword } = this.state;
+    return (
+      <div>
+        <div>
+          <div>
+            <div>
+              <br />
+              <br />
+              <center>
+                <h3>Admin Login</h3>
+              </center>
+              <br />
+              <br />
             </div>
 
-        );
-    }
+            <div>
+              <div>
+                <center>
+                  <input
+                    type="text"
+                    name="uname"
+                    value={uname}
+                    onChange={this.onChange}
+                    autoComplete="off"
+                    placeholder="Username"
+                    style={{ border: "2px solid gray",borderRadius: '8px', padding: "8px" }}
+                  />
+                </center>
+              </div>
+              <br />
 
+              <div>
+                <center>
+                  <input
+                    type="password"
+                    id="password"
+                    name="pword"
+                    autoComplete="off"
+                    value={pword}
+                    onChange={this.onChange}
+                    placeholder="Password"
+                    style={{ border: "2px solid gray",borderRadius: '8px', padding: "8px" }}
+                  />
+                </center>
+              </div>
+              <br />
+
+              <div>
+                <p>
+                  <center>
+                    <button style={{
+                        padding:'8px',
+                        borderRadius:'8px',
+                        backgroundColor:'black',
+                        color:'white'
+                    }} onClick={this.onSubmit}>Login</button>
+                  </center>
+                </p>
+              </div>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default AdminLogin;
